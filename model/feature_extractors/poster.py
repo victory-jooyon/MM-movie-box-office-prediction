e5766afafe8b135de2e1f3118eb7b95990a9467d
one-road <hangil.park@kaist.ac.kr>
2020-11-26 05:10:32 +0900

    Add : feature_extractors and multi-modal model
    - Using naive concat
    - Pre-processed data required

---
+++ model/feature_extractors/poster.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torchvision
+
+class PosterFeatureExtractor(nn.Module):
+    def __init__(self, feature_size):
+        super(PosterFeatureExtractor, self).__init__()
+        self.feature_extractor = torchvision.models.resnet18(pretrained=True)
+        for param in self.feature_extractor.parameters():
+            param.requires_grad = False
+        num_ftrs = self.feature_extractor.fc.in_features
+        self.feature_extractor.fc = nn.Linear(num_ftrs, feature_size)
+
+    def forward(self, x):
+        #[768]
+        output = self.feature_extractor(x)
+        return output
--- model/model.py
+++ model/model.py
@@ -1,5 +1,29 @@
+import torch
 import torch.nn as nn
+from model.feature_extractors import TMDBFeatureExtractor, PosterFeatureExtractor, IMDBFeatureExtractor
 
-class SomeModel(nn.Module):
-    def __init__(self):
-        super(SomeModel, self).__init__()
+class MultimodalPredictionModel(nn.Module):
+    def __init__(self, feature_size, hidden_layer_size):
+        super(MultimodalPredictionModel, self).__init__()
+        self.tmdb = TMDBFeatureExtractor(feature_size)
+        self.poster = PosterFeatureExtractor(feature_size)
+        self.imdb = IMDBFeatureExtractor(feature_size)
+
+        self.fc = nn.Sequential(nn.Linear(3 * feature_size, hidden_layer_size),
+                                nn.ReLU(),
+                                nn.dropout(),
+                                nn.Linear(hidden_layer_size, 1))
+
+    def forward(self, tmdb_tok, tmdb_seg, poster_input, imdb_tok, imdb_seg):
+        #[feature_size]
+        tmdb_feature = self.tmdb(tmdb_tok, tmdb_seg)
+        poster_feature = self.poster(poster_input)
+        tmdb_feature = self.tmdb(imdb_tok, imdb_seg)
+
+        #[feature_size * 3]
+        total_feature = torch.cat((tmdb_feature, poster_feature, tmdb_feature), dim=0)
+
+         #[1]
+        output = self.fc(total_feature)
+
+        return output
